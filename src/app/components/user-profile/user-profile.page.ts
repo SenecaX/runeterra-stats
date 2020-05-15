74f56d58eb78ab48c43843a1b71ebe658e32cb50
AeonX <kedaren.dorasami@gmail.com>
2020-05-15 13:20:39 +0400

    added proxy

--- src/app/components/user-profile/user-profile.page.ts
+++ src/app/components/user-profile/user-profile.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from "@angular/core";
+import { RuneterraService } from "src/app/services/runeterra.service";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.page.html',
-  styleUrls: ['./user-profile.page.scss'],
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.page.html",
+  styleUrls: ["./user-profile.page.scss"],
 })
-export class UserProfilePage implements OnInit {
+export class UserProfilePage implements OnInit, OnChanges {
+  constructor(public runeterraService: RuneterraService) {
+    let obj = this.runeterraService.testingApi();
 
-  constructor() { }
-
-  ngOnInit() {
+    console.log("obj", obj);
   }
 
+  ngOnInit() {
+    console.log("entered init");
+    // this.runeterraService.testingApi();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("entered", changes);
+  }
 }
